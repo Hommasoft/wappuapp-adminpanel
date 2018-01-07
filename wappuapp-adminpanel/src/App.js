5677415297d504c7e31f895a7330d9e3cd076fab
Jeva <jeva92@gmail.com>
2018-01-07 13:24:04 +0200

    WA-91 Tidied the layout out and added confirmation boxes

--- wappuapp-adminpanel/src/App.js
+++ wappuapp-adminpanel/src/App.js
@@ -1,24 +1,49 @@
 import React, { Component } from 'react';
-import { Col, Jumbotron, Nav, NavItem, Row, Grid } from 'react-bootstrap';
+import {
+  Col,
+  Jumbotron,
+  Nav,
+  NavItem,
+  Row,
+  Grid,
+  NavDropdown,
+  MenuItem,
+  Well
+} from 'react-bootstrap';
+
+import History from './history';
 
 class App extends Component {
   render() {
     const admin = localStorage.getItem('admin');
+    const currentPage = History.location.pathname;
     if (admin === 'true') {
       return (
         <Grid>
           <Row>
             <Col md={10} sm={12}>
               <Jumbotron>
-                <Nav bsStyle="tabs">
-                  <NavItem href="/">Front</NavItem>
-                  <NavItem href="/logout">Logout</NavItem>
-                  <NavItem href="/feed">Feed</NavItem>
-                  <NavItem href="/moderatorlist">ModList</NavItem>
-                  <NavItem href="/changepassword">ChangePassword</NavItem>
+                <Nav justified bsStyle="tabs" activeKey={currentPage}>
+                  <NavItem eventKey="/" href="/">
+                    Front
+                  </NavItem>
+                  <NavItem eventKey="/feed" href="/feed">
+                    Feed
+                  </NavItem>
+                  <NavItem eventKey="/moderatorlist" href="/moderatorlist">
+                    ModList
+                  </NavItem>
+                  <NavDropdown title="Account" id="nav-dropdown">
+                    <MenuItem href="/changepassword">Change Password</MenuItem>
+                    <MenuItem href="/logout">Logout</MenuItem>
+                  </NavDropdown>
                 </Nav>
+                <br />
+                <br />
                 <Row>
-                  <Col xs={12}>{this.props.children}</Col>
+                  <Col xs={12}>
+                    <Well>{this.props.children}</Well>
+                  </Col>
                 </Row>
               </Jumbotron>
             </Col>
@@ -31,14 +56,24 @@ class App extends Component {
           <Row>
             <Col md={10} sm={12}>
               <Jumbotron>
-                <Nav bsStyle="tabs">
-                  <NavItem href="/">Front</NavItem>
-                  <NavItem href="/logout">Logout</NavItem>
-                  <NavItem href="/feed">Feed</NavItem>
-                  <NavItem href="/changepassword">ChangePassword</NavItem>
+                <Nav justified bsStyle="tabs" activeKey={currentPage}>
+                  <NavItem eventKey="/" href="/">
+                    Front
+                  </NavItem>
+                  <NavItem eventKey="/feed" href="/feed">
+                    Feed
+                  </NavItem>
+                  <NavDropdown title="Account" id="nav-dropdown">
+                    <MenuItem href="/changepassword">Change Password</MenuItem>
+                    <MenuItem href="/logout">Logout</MenuItem>
+                  </NavDropdown>
                 </Nav>
+                <br />
+                <br />
                 <Row>
-                  <Col xs={12}>{this.props.children}</Col>
+                  <Col xs={12}>
+                    <Well>{this.props.children}</Well>
+                  </Col>
                 </Row>
               </Jumbotron>
             </Col>
@@ -51,12 +86,20 @@ class App extends Component {
           <Row>
             <Col md={10} sm={12}>
               <Jumbotron>
-                <Nav bsStyle="tabs">
-                  <NavItem href="/">Front</NavItem>
-                  <NavItem href="/login">Login</NavItem>
+                <Nav justified bsStyle="tabs" activeKey={currentPage}>
+                  <NavItem eventKey="/" href="/">
+                    Front
+                  </NavItem>
+                  <NavItem eventKey="/login" href="/login">
+                    Login
+                  </NavItem>
                 </Nav>
+                <br />
+                <br />
                 <Row>
-                  <Col xs={12}>{this.props.children}</Col>
+                  <Col xs={12}>
+                    <Well>{this.props.children}</Well>
+                  </Col>
                 </Row>
               </Jumbotron>
             </Col>
