d7ed731f6b154e613818fcdaf69a4195ddc7039f
Mikael Lagerbom <tom.lagerbom@student.tut.fi>
2018-02-01 10:21:26 +0200

    removed frontend dockerization, added create-react-app proxy in favor of manual nginx, started crying uncontrollably

--- wappuapp-adminpanel/src/services/api.js
+++ wappuapp-adminpanel/src/services/api.js
@@ -1,11 +1,6 @@
 import axios from 'axios';
 
-const baseUrl =
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:9000/api'
-    : process.env.REACT_APP_BACKEND_URI;
-
-console.log(baseUrl);
+const baseUrl = '/api';
 
 const api = axios.create({
   baseURL: baseUrl
