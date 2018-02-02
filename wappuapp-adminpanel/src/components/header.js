396d746fc3dc4c698252d9e238cdfe3f92e20947
Jeva <jeva92@gmail.com>
2018-02-02 10:23:54 +0200

    Initial header

--- wappuapp-adminpanel/src/components/header.js
+++ wappuapp-adminpanel/src/components/header.js
@@ -3,6 +3,7 @@ import { Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
 import History from '../history';
 import '../assets/css/header.css';
+import logo from '../assets/img/logo.png';
 
 class Header extends Component {
   render() {
@@ -12,57 +13,69 @@ class Header extends Component {
     if (admin === 'true') {
       return (
         <div className="headerBar">
-          <Nav justified bsStyle="tabs" activeKey={currentPage}>
-            <NavItem eventKey="/" href="/">
-              Front
-            </NavItem>
-            <NavItem eventKey="/feed" href="/feed">
-              Feed
-            </NavItem>
-            <NavItem eventKey="/event" href="/event/0">
-              Event
-            </NavItem>
-            <NavItem eventKey="/moderatorlist" href="/moderatorlist">
-              ModList
-            </NavItem>
-            <NavDropdown eventKey="/account" title="Account" id="nav-dropdown">
-              <MenuItem href="/account">Account Page</MenuItem>
-              <MenuItem href="/logout">Logout</MenuItem>
-            </NavDropdown>
-          </Nav>
+          <div className="headerContentLogoLeft">
+            <img src={logo} alt="logo" />
+          </div>
+          <div className="headerContent">
+            <Nav justified bsStyle="tabs" activeKey={currentPage}>
+              <NavItem eventKey="/" href="/">
+                Front
+              </NavItem>
+              <NavItem eventKey="/feed" href="/feed">
+                Feed
+              </NavItem>
+              <NavItem eventKey="/event" href="/event/0">
+                Event
+              </NavItem>
+              <NavItem eventKey="/moderatorlist" href="/moderatorlist">
+                ModList
+              </NavItem>
+              <NavDropdown eventKey="/account" title="Account" id="nav-dropdown">
+                <MenuItem href="/account">Account Page</MenuItem>
+                <MenuItem href="/logout">Logout</MenuItem>
+              </NavDropdown>
+            </Nav>
+          </div>
+          <div className="headerContentLogoRight">
+            <img src={logo} alt="logo" />
+          </div>
         </div>
       );
     } else if (admin === 'false') {
       return (
-        <div>
-          <Nav justified bsStyle="tabs" activeKey={currentPage}>
-            <NavItem eventKey="/" href="/">
-              Front
-            </NavItem>
-            <NavItem eventKey="/feed" href="/feed">
-              Feed
-            </NavItem>
-            <NavItem eventKey="/event" href="/event/0">
-              Event
-            </NavItem>
-            <NavDropdown eventKey="/account" title="Account" id="nav-dropdown">
-              <MenuItem href="/account">Account Page</MenuItem>
-              <MenuItem href="/logout">Logout</MenuItem>
-            </NavDropdown>
-          </Nav>
+        <div className="headerBar">
+          <div className="headerContent">
+            <Nav justified bsStyle="tabs" activeKey={currentPage}>
+              <NavItem eventKey="/" href="/">
+                Front
+              </NavItem>
+              <NavItem eventKey="/feed" href="/feed">
+                Feed
+              </NavItem>
+              <NavItem eventKey="/event" href="/event/0">
+                Event
+              </NavItem>
+              <NavDropdown eventKey="/account" title="Account" id="nav-dropdown">
+                <MenuItem href="/account">Account Page</MenuItem>
+                <MenuItem href="/logout">Logout</MenuItem>
+              </NavDropdown>
+            </Nav>
+          </div>
         </div>
       );
     } else {
       return (
-        <div>
-          <Nav justified bsStyle="tabs" activeKey={currentPage}>
-            <NavItem eventKey="/" href="/">
-              Front
-            </NavItem>
-            <NavItem eventKey="/login" href="/login">
-              Login
-            </NavItem>
-          </Nav>
+        <div className="headerBar">
+          <div className="headerContent">
+            <Nav justified bsStyle="tabs" activeKey={currentPage}>
+              <NavItem eventKey="/" href="/">
+                Front
+              </NavItem>
+              <NavItem eventKey="/login" href="/login">
+                Login
+              </NavItem>
+            </Nav>
+          </div>
         </div>
       );
     }
